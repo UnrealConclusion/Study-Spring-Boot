0e1dc08b4a2e251216f1590251954ed99a40ea29
Jacob Lin <jacoblin@Jacobs-MacBook-Air.local>
2025-11-12 00:53:14 -0500

    learned One to Many Uni-directional mapping

---
+++ 09 Advanced Mappings/04 OneToMany Uni-directional/src/main/java/com/unrealconclusion/OneToOne/dao/AppDAO.java
@@ -0,0 +1,23 @@
+package com.unrealconclusion.OneToOne.dao;
+
+import java.util.List;
+
+import com.unrealconclusion.OneToOne.entity.Course;
+import com.unrealconclusion.OneToOne.entity.Instructor;
+import com.unrealconclusion.OneToOne.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+    Instructor findInstructorById(int id);
+    void deleteInstructorById(int id);
+    InstructorDetail findInstructorDetailById(int id);
+    void deleteInstructorDetailById(int id);
+    List<Course> findCoursesByInstructorId(int id);
+    Instructor findInstructorByIdJoinFetch(int id);
+    void update(Instructor instructor);
+    void update(Course course);
+    Course findCourseById(int id);
+    void deleteCourseById(int id);
+    void save(Course course);
+    Course findCourseAndReviewById(int id);
+}
